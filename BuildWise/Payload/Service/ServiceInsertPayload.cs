5b08d2df5fe3d2749e9bac3817fac16ac12df0ef
Laisa Cazarotto <laisacaz@hotmail.com>
2024-05-05 22:58:17 -0300

    Adiciona payload

---
+++ BuildWise/Payload/Service/ServiceInsertPayload.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildWise.Payload.Service
+{
+    public class ServiceInsertPayload
+    {
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
